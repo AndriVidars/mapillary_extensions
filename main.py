44500be080a023c22226050026fb8103c9136d12
AndriVidars <andri.vidars@gmail.com>
2026-01-20 18:03:37 +0000

    surpress mapillary logging for vercel read only file system

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from fastapi import FastAPI
+import os
+import logging
+from fastapi import FastAPI, HTTPException
+
+# Vercel Read-Only Filesystem Fix: Completely disable Mapillary's file-based logging
+# 1. Mock os.makedirs to prevent errors when Mapillary attempts to create a 'logs' directory
+original_makedirs = os.makedirs
+def hooked_makedirs(name, mode=0o777, exist_ok=False):
+    if "mapillary" in name:
+        return
+    return original_makedirs(name, mode, exist_ok)
+os.makedirs = hooked_makedirs
+
+# 2. Mock logging.FileHandler so Mapillary doesn't try to open any log files
+class NullFileHandler(logging.NullHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+    def setFormatter(self, *args, **kwargs):
+        pass
+logging.FileHandler = NullFileHandler
+
 import mapillary.interface as mly
 import random
 
